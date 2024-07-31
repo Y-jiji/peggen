a7c82a6d3660f9d5902b6be9561b97db8c90d26a
Y-jiji <y.jijiji.data.science@gmail.com>
2024-07-31 15:29:18 +0800

    Feature: fill in more examples.

--- pigeon-test/src/json.rs
+++ pigeon-test/src/json.rs
@@ -6,12 +6,15 @@ use pigeon::{AstImpl, Num, ParseImpl, Prepend, Space};
 pub enum Json {
     #[rule("{0:`[0-9]|([1-9][0-9]*)`}")]
     Int(u64),
+    #[rule("{0:`false|true`}")]
+    Bool(bool),
     #[rule(r"\{ [*0: {0:`[a-zA-Z]+`} : {1} , ][?0: {0:`[a-zA-Z]+`} : {1} ] \}")]
     Obj(RVec<(String, Json)>),
     #[rule(r"\[ [*0: {0} , ][?0: {0} ] \]")]
     Arr(RVec<Json>),
 }
 
+// TODO: put this into pigeon-impl
 #[derive(Prepend)]
 pub struct RVec<T>(Vec<T>);
 
@@ -38,7 +41,7 @@ mod test {
 
     #[test]
     fn json() {
-        let json = r"{x: 1, y: [2, 3]}";
+        let json = r"{x: 1, y: [2, 3], z: false}";
         let json = Parser::<Json>::parse(json).unwrap();
         println!("{json:?}");
     }
