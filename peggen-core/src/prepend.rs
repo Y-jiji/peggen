e2f9f1b5a65fed8fb309a863c91d7c161664f6b7
Y-jiji <y.jijiji.data.science@gmail.com>
2024-09-18 23:09:30 -0400

    Feature: add implementation for option.

--- peggen-core/src/prepend.rs
+++ peggen-core/src/prepend.rs
@@ -1,5 +1,38 @@
+use crate::*;
+
 pub trait Prepend<Extra: Copy> {
     type Item;
     fn empty(with: Extra) -> Self;
     fn prepend(&mut self, value: Self::Item, with: Extra);
+}
+
+impl<T, Extra: Copy> Prepend<Extra> for Option<T> {
+    type Item = T;
+    fn empty(_: Extra) -> Self {
+        Self::None
+    }
+    fn prepend(&mut self, value: Self::Item, _: Extra) {
+        *self = Some(value)
+    }
+}
+
+impl<T, Extra: Copy> AstImpl<Extra> for Option<T> where
+    Self: Prepend<Extra>,
+    <Self as Prepend<Extra>>::Item: AstImpl<Extra>
+{
+    fn ast<'lifetime>(
+        input: &'lifetime str, 
+        stack: &'lifetime [Tag], 
+        with: Extra
+    ) -> (&'lifetime [Tag], Self) {
+        let tag = &stack[stack.len()-1];
+        let mut stack = &stack[..stack.len()-1];
+        let mut this = <Self as Prepend<Extra>>::empty(with);
+        for _ in 0..tag.rule {
+            let (stack_, value) = <<Self as Prepend<Extra>>::Item as AstImpl<Extra>>::ast(input, stack, with);
+            this.prepend(value, with);
+            stack = stack_;
+        }
+        (stack, this)
+    }
 }
